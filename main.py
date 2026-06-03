db8a2c2359475506d81956d8a9dfe8f51769228b
Jasurbek-Olimjonov <olimjonovzilxa@gmail.com>
2026-06-03 19:43:53 +0900

    remove conf

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ BASE_WEBHOOK_URL = conf.web.url
 @dp.startup()
 async def startup(bot: Bot):
     await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
-    await bot_settings(bot)
+    # await bot_settings(bot)
     await db.create_all()
     os.makedirs('media', exist_ok=True)
     os.makedirs('locales', exist_ok=True)
